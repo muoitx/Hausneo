80ca7a45caee8f72399bae0f6879d0f2606c2dde
MUOITX\PC <muoitm.pnv@gmail.com>
2017-10-04 14:32:08 +0700

    Update PC top page

---
+++ common/js/common.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$(document).on('click', '.btn_header', function(){
+		if(!$(this).hasClass('active')){
+			$(this).addClass('active');
+			$("#header").addClass('active');
+			$("#content").addClass('active');
+		} else {
+			$(this).removeClass('active');
+			$("#header").removeClass('active');
+			$("#content").removeClass('active');
+		}
+	});
+
+	$("#slider .main_slider").vegas({
+		timer: false,
+		delay: 7000,
+		transitionDuration: 2000,
+		transition: "random",
+    slides: [
+      { src: "images/slider.jpg" },
+      { src: "images/slider_1.jpg" }
+    ]
+	});
+});
